cfd9caedda0fca57cdcb58853b2a20d1d2522e5a
weishijun <weishijun@msn.com>
2012-02-17 12:20:21 +0800

    trad chinese resources

---
+++ src/wiznote_zh_TW.ts
@@ -0,0 +1,360 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_TW">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="aboutdialog.ui" line="14"/>
+        <source>About WizNote</source>
+        <translation type="unfinished">关于为知笔记</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="58"/>
+        <source>WizNote 0.01</source>
+        <translation type="unfinished">爲知筆記 0.01</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="71"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a href=&quot;http://www.wiz.cn/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://wiz.cn/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.cpp" line="19"/>
+        <source>WizNote for Mac %1</source>
+        <translation type="unfinished">爲知筆記 Mac版 %1</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.cpp" line="21"/>
+        <source>WizNote for Linux %1</source>
+        <translation type="unfinished">爲知筆記 Linux版 %1</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.cpp" line="23"/>
+        <source>WizNote for Windows %1</source>
+        <translation type="unfinished">爲知筆記Windows版 %1</translation>
+    </message>
+</context>
+<context>
+    <name>CWizCategoryView</name>
+    <message>
+        <location filename="wizcategoryview.cpp" line="285"/>
+        <source>Note Folders</source>
+        <translation type="unfinished">新建檔案夾</translation>
+    </message>
+    <message>
+        <location filename="wizcategoryview.cpp" line="348"/>
+        <source>Tags</source>
+        <translation type="unfinished">標簽</translation>
+    </message>
+    <message>
+        <location filename="wizcategoryview.cpp" line="372"/>
+        <source>Trash</source>
+        <translation type="unfinished">回收桶</translation>
+    </message>
+    <message>
+        <location filename="wizcategoryview.cpp" line="400"/>
+        <location filename="wizcategoryview.cpp" line="411"/>
+        <source>New Folder</source>
+        <translation type="unfinished">新建檔案夾</translation>
+    </message>
+    <message>
+        <location filename="wizcategoryview.cpp" line="412"/>
+        <source>Delete Folder</source>
+        <translation type="unfinished">刪除檔案夾</translation>
+    </message>
+    <message>
+        <location filename="wizcategoryview.cpp" line="423"/>
+        <location filename="wizcategoryview.cpp" line="434"/>
+        <source>New Tag</source>
+        <translation type="unfinished">新建標簽</translation>
+    </message>
+    <message>
+        <location filename="wizcategoryview.cpp" line="435"/>
+        <source>Delete Tag</source>
+        <translation type="unfinished">刪除標簽</translation>
+    </message>
+    <message>
+        <location filename="wizcategoryview.cpp" line="446"/>
+        <source>Empty</source>
+        <translation type="unfinished">空</translation>
+    </message>
+</context>
+<context>
+    <name>CWizDatabase</name>
+    <message>
+        <location filename="share/wizdatabase.cpp" line="977"/>
+        <source>My Notes</source>
+        <translation type="unfinished">我的筆記</translation>
+    </message>
+    <message>
+        <location filename="share/wizdatabase.cpp" line="981"/>
+        <source>My Journals</source>
+        <translation type="unfinished">我的日記</translation>
+    </message>
+    <message>
+        <location filename="share/wizdatabase.cpp" line="985"/>
+        <source>My Events</source>
+        <translation type="unfinished">我的事件</translation>
+    </message>
+    <message>
+        <location filename="share/wizdatabase.cpp" line="989"/>
+        <source>My Sticky Notes</source>
+        <translation type="unfinished">我的桌面便箋</translation>
+    </message>
+    <message>
+        <location filename="share/wizdatabase.cpp" line="993"/>
+        <source>My Emails</source>
+        <translation type="unfinished">我的郵件</translation>
+    </message>
+    <message>
+        <location filename="share/wizdatabase.cpp" line="997"/>
+        <source>My Drafts</source>
+        <translation type="unfinished">我的草稿</translation>
+    </message>
+    <message>
+        <location filename="share/wizdatabase.cpp" line="1003"/>
+        <source>Inbox</source>
+        <translation type="unfinished">收集箱</translation>
+    </message>
+    <message>
+        <location filename="share/wizdatabase.cpp" line="1007"/>
+        <source>Completed</source>
+        <translation type="unfinished">已完成</translation>
+    </message>
+</context>
+<context>
+    <name>CreateAccountDialog</name>
+    <message>
+        <location filename="createaccountdialog.ui" line="14"/>
+        <source>Create Account</source>
+        <translation type="unfinished">創建賬戶</translation>
+    </message>
+    <message>
+        <location filename="createaccountdialog.ui" line="26"/>
+        <source>&amp;User ID (Email):</source>
+        <oldsource>&amp;User ID: (Email)</oldsource>
+        <translation type="unfinished">用戶ID(&amp;U):</translation>
+    </message>
+    <message>
+        <location filename="createaccountdialog.ui" line="52"/>
+        <source>&amp;Password</source>
+        <translation type="unfinished">密碼(&amp;P):</translation>
+    </message>
+    <message>
+        <location filename="createaccountdialog.ui" line="94"/>
+        <source>&amp;Confirm Password:</source>
+        <translation type="unfinished">確認密碼(&amp;C):</translation>
+    </message>
+    <message>
+        <location filename="createaccountdialog.cpp" line="39"/>
+        <source>Please enter user id</source>
+        <translation type="unfinished">請輸入用戶ID</translation>
+    </message>
+    <message>
+        <location filename="createaccountdialog.cpp" line="44"/>
+        <source>Please enter user password</source>
+        <translation type="unfinished">請輸入用戶密碼</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="56"/>
+        <source>WizNote</source>
+        <translation type="unfinished">爲知筆記</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="115"/>
+        <source>Search</source>
+        <translation type="unfinished">搜索</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="115"/>
+        <source>Search your notes</source>
+        <translation type="unfinished">搜索您的筆記</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="319"/>
+        <source>New note</source>
+        <translation type="unfinished">新建筆記</translation>
+    </message>
+</context>
+<context>
+    <name>NewFolderDialog</name>
+    <message>
+        <location filename="newfolderdialog.ui" line="20"/>
+        <source>New Folder</source>
+        <translation type="unfinished">新建檔案夾</translation>
+    </message>
+    <message>
+        <location filename="newfolderdialog.ui" line="42"/>
+        <source>&amp;Folder name:</source>
+        <translation type="unfinished">檔案夾名稱(&amp;F):</translation>
+    </message>
+</context>
+<context>
+    <name>NewTagDialog</name>
+    <message>
+        <location filename="newtagdialog.ui" line="14"/>
+        <source>New Tag</source>
+        <translation type="unfinished">新建標簽</translation>
+    </message>
+    <message>
+        <location filename="newtagdialog.ui" line="26"/>
+        <source>&amp;Tag name:</source>
+        <translation type="unfinished">標簽名稱(&amp;T):</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="15"/>
+        <source>WizNote</source>
+        <translation type="unfinished">爲知筆記</translation>
+    </message>
+    <message>
+        <location filename="wizactions.cpp" line="28"/>
+        <source>&amp;File</source>
+        <translation type="unfinished">文件(&amp;F)</translation>
+    </message>
+    <message>
+        <location filename="wizactions.cpp" line="29"/>
+        <source>&amp;Tools</source>
+        <translation type="unfinished">工具(&amp;T)</translation>
+    </message>
+    <message>
+        <location filename="wizactions.cpp" line="30"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished">幫助(&amp;H)</translation>
+    </message>
+    <message>
+        <location filename="wizactions.cpp" line="37"/>
+        <source>Exit</source>
+        <translation type="unfinished">退出</translation>
+    </message>
+    <message>
+        <location filename="wizactions.cpp" line="38"/>
+        <source>Logout</source>
+        <translation type="unfinished">登出</translation>
+    </message>
+    <message>
+        <location filename="wizactions.cpp" line="39"/>
+        <source>About WizNote...</source>
+        <translation type="unfinished">關於爲知筆記...</translation>
+    </message>
+    <message>
+        <location filename="wizactions.cpp" line="40"/>
+        <source>Delete Note</source>
+        <translation type="unfinished">刪除筆記</translation>
+    </message>
+    <message>
+        <location filename="wizactions.cpp" line="41"/>
+        <source>Sync</source>
+        <translation type="unfinished">同步</translation>
+    </message>
+    <message>
+        <location filename="wizactions.cpp" line="42"/>
+        <source>New Note</source>
+        <translation type="unfinished">新建筆記</translation>
+    </message>
+    <message>
+        <location filename="wizactions.cpp" line="43"/>
+        <location filename="wizactions.cpp" line="44"/>
+        <source>Capture Screen</source>
+        <translation type="unfinished">屏幕截圖</translation>
+    </message>
+    <message>
+        <location filename="wizactions.cpp" line="45"/>
+        <source>Capture Screen (Hide WizNote)</source>
+        <translation type="unfinished">屏幕截圖（隱藏爲知筆記）</translation>
+    </message>
+    <message>
+        <location filename="wizactions.cpp" line="46"/>
+        <source>Menu</source>
+        <translation type="unfinished">選單</translation>
+    </message>
+    <message>
+        <location filename="wizactions.cpp" line="47"/>
+        <source>Back</source>
+        <translation type="unfinished">後退</translation>
+    </message>
+    <message>
+        <location filename="wizactions.cpp" line="48"/>
+        <source>Forward</source>
+        <translation type="unfinished">前進</translation>
+    </message>
+    <message>
+        <location filename="wizcategoryview.cpp" line="230"/>
+        <source>Search for %1</source>
+        <translation type="unfinished">搜索 %1 結果</translation>
+    </message>
+</context>
+<context>
+    <name>QuaZipFilePrivate</name>
+    <message>
+        <location filename="zip/quazipfile.cpp" line="172"/>
+        <source>ZIP/UNZIP API error %1</source>
+        <translation type="unfinished">ZIP/UNZIP API 錯誤r %1</translation>
+    </message>
+</context>
+<context>
+    <name>WelcomeDialog</name>
+    <message>
+        <location filename="welcomedialog.ui" line="14"/>
+        <source>Welcome to WizNote</source>
+        <translation type="unfinished">歡迎使用微址筆記</translation>
+    </message>
+    <message>
+        <location filename="welcomedialog.ui" line="29"/>
+        <source>&amp;User ID (Email):</source>
+        <oldsource>&amp;User ID:</oldsource>
+        <translation type="unfinished">用戶ID(&amp;U):</translation>
+    </message>
+    <message>
+        <location filename="welcomedialog.ui" line="74"/>
+        <source>&amp;Password:</source>
+        <translation type="unfinished">密碼(&amp;P):</translation>
+    </message>
+    <message>
+        <location filename="welcomedialog.ui" line="107"/>
+        <source>about:blank</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="welcomedialog.cpp" line="64"/>
+        <source>Please enter user id</source>
+        <translation type="unfinished">請輸入用戶ID</translation>
+    </message>
+    <message>
+        <location filename="welcomedialog.cpp" line="69"/>
+        <source>Please enter user password</source>
+        <translation type="unfinished">請輸入用戶密碼</translation>
+    </message>
+</context>
+<context>
+    <name>WizDownloadObjectDataDialog</name>
+    <message>
+        <location filename="share/wizdownloadobjectdatadialog.ui" line="20"/>
+        <source>Downloading Object</source>
+        <translation type="unfinished">正在下載數據</translation>
+    </message>
+    <message>
+        <location filename="share/wizdownloadobjectdatadialog.ui" line="48"/>
+        <source>Downloading %1</source>
+        <translation type="unfinished">正在下載 %1</translation>
+    </message>
+    <message>
+        <location filename="share/wizdownloadobjectdatadialog.cpp" line="71"/>
+        <source>Failed to download data!
+
+%1</source>
+        <translation type="unfinished">下載數據失敗
+
+%1</translation>
+    </message>
+</context>
+</TS>
