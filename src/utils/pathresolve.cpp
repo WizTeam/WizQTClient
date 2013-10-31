3b165d6378ba8382efc656edf03f2b7f1b59f056
Albert.Zhou <albert.zhou@wiz.cn>
2013-10-31 15:01:37 +0800

    Add markdown plugin files
    1. new PathResolve class
    2. add markdown plugin files

---
+++ src/utils/pathresolve.cpp
@@ -0,0 +1,66 @@
+#include "pathresolve.h"
+
+#include <QApplication>
+#include <QDir>
+
+namespace Utils {
+
+QString PathResolve::appPath()
+{
+    QString strPath = QApplication::applicationDirPath();
+    addBackslash(strPath);
+    return strPath;
+}
+
+QString PathResolve::resourcesPath()
+{
+#ifdef Q_OS_MAC
+    QDir dir(appPath());
+    dir.cdUp();
+    dir.cd("Resources");
+    QString strPath = dir.path();
+    addBackslash(strPath);
+    return strPath;
+#elif defined(Q_OS_LINUX)
+    QDir dir(appPath());
+    dir.cdUp();
+    dir.cd("share/wiznote");
+    QString strPath = dir.path();
+    addBackslash(strPath);
+    return strPath;
+#else
+    return appPath();
+#endif
+}
+
+QString PathResolve::dataStorePath()
+{
+    QString strPath = QDir::homePath();
+    strPath += "/.wiznote/";
+
+    ensurePathExists(strPath);
+    return strPath;
+}
+
+QString PathResolve::pluginsPath()
+{
+    return resourcesPath() + "plugins/";
+}
+
+void PathResolve::addBackslash(QString& strPath)
+{
+    strPath.replace('\\', '/');
+
+    if (strPath.endsWith('/'))
+        return;
+
+    strPath += '/';
+}
+
+void PathResolve::ensurePathExists(const QString& path)
+{
+    QDir dir;
+    dir.mkpath(path);
+}
+
+} // namespace Utils
