4e287ab65ded6c88bda056b917e8ba7dc8357333
wizweishijun <wsj@wiz.cn>
2014-03-19 19:12:30 +0800

    sleep for main thread

--- src/wizmainwindow.cpp
+++ src/wizmainwindow.cpp
@@ -175,6 +175,15 @@ void MainWindow::on_application_aboutToQuit()
     cleanOnQuit();
 }
 
+class SleepThread : public QThread
+{
+ public :
+     static void sleep(long iSleepTime)
+     {
+          QThread::sleep(iSleepTime);
+     }
+};
+
 void MainWindow::cleanOnQuit()
 {
     m_category->saveState();
@@ -193,7 +202,7 @@ void MainWindow::cleanOnQuit()
         {
             if (m_sync->isFinished())
                 break;
-            sleep(1);
+            SleepThread::sleep(1);
             QApplication::processEvents();
         }
     }
