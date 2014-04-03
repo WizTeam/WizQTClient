2711484fbc4e8c55b90c2a89d5dab4d572bf47ba
WizLxn <lxn@wiz.com>
2014-04-03 18:57:16 +0800

    BizServiceExpr flag

--- src/share/wizobject.cpp
+++ src/share/wizobject.cpp
@@ -749,7 +749,7 @@ bool WIZBIZDATA::LoadFromXmlRpc(CWizXmlRpcStructValue& data)
     data.GetStr(_T("biz_guid"), bizGUID);
     data.GetInt(_T("user_group"), bizUserRole);
     data.GetInt(_T("biz_level"), bizLevel);
-    data.GetBool(_T("biz_is_due"), bizIsDue);
+    data.GetBool(_T("is_due"), bizIsDue);
 
     return !bizGUID.isEmpty()
             && !bizName.isEmpty();
