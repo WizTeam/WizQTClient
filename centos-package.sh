48d14b85a8ae37db41cf683f7b2f23c674f6b60c
WizLxn <lxn@wiz.cn>
2014-06-23 14:41:25 +0800

    compile on centos

--- centos-package.sh
+++ centos-package.sh
@@ -4,7 +4,7 @@ rm -rf ../WizQTClient-Release-Linux/*
 
 cd ../WizQTClient-Release-Linux
 
-cmake -DWIZNOTE_USE_QT5=NO -DCMAKE_BUILD_TYPE=Release ../WizQTClient -DQT_QMAKE_EXECUTABLE=/usr/local/Trolltech/Qt-4.8.5/bin/qmake  && \
+cmake -DWIZNOTE_USE_QT5=NO -DCMAKE_BUILD_TYPE=Release ../WizQTClient -DQT_QMAKE_EXECUTABLE=/usr/local/Trolltech/Qt-4.8.6/bin/qmake  && \
 make -j5
 
 cd ..
@@ -15,12 +15,17 @@ mkdir bin
 cd bin
 
 cp ../../WizQTClient-Release-Linux/bin/WizNote ./
-cp /usr/local/Trolltech/Qt-4.8.5/lib/libQtWebKit.so.4 ./
-cp /usr/local/Trolltech/Qt-4.8.5/lib/libQtGui.so.4 ./
-cp /usr/local/Trolltech/Qt-4.8.5/lib//libQtXml.so.4 ./
-cp /usr/local/Trolltech/Qt-4.8.5/lib/libQtNetwork.so.4 ./
-cp /usr/local/Trolltech/Qt-4.8.5/lib/libQtCore.so.4 ./
-cp /usr/local/Trolltech/Qt-4.8.5/lib/libQtXmlPatterns.so.4 ./
+cp /usr/local/Trolltech/Qt-4.8.6/lib/libQtWebKit.so.4 ./
+cp /usr/local/Trolltech/Qt-4.8.6/lib/libQtGui.so.4 ./
+cp /usr/local/Trolltech/Qt-4.8.6/lib//libQtXml.so.4 ./
+cp /usr/local/Trolltech/Qt-4.8.6/lib/libQtNetwork.so.4 ./
+cp /usr/local/Trolltech/Qt-4.8.6/lib/libQtCore.so.4 ./
+cp /usr/local/Trolltech/Qt-4.8.6/lib/libQtXmlPatterns.so.4 ./
+cp ../../WizQTClient-Release-Linux/lib/wiznote/plugins/libextensionsystem.so ./
+cp ../../WizQTClient-Release-Linux/lib/wiznote/plugins/libaggregation.so ./
+cp ../../WizQTClient-Release-Linux/lib/wiznote/plugins/libCore.so ./
+cp ../../WizQTClient-Release-Linux/lib/wiznote/plugins/libMarkDown.so ./
+cp ../../WizQTClient-Release-Linux/lib/wiznote/plugins/libHelloWorld.so ./
 
 cd ..
 mkdir lib
@@ -30,10 +35,6 @@ cd wiznote
 mkdir plugins
 cd plugins
 
-cp ../../../../WizQTClient-Release-Linux/lib/wiznote/plugins/libextensionsystem.so ./
-cp ../../../../WizQTClient-Release-Linux/lib/wiznote/plugins/libaggregation.so ./
-cp ../../../../WizQTClient-Release-Linux/lib/wiznote/plugins/libCore.so ./
-cp ../../../../WizQTClient-Release-Linux/lib/wiznote/plugins/libMarkDown.so ./
 cp ../../../../WizQTClient-Release-Linux/lib/wiznote/plugins/MarkDown.pluginspec ./
 cp ../../../../WizQTClient-Release-Linux/lib/wiznote/plugins/Core.pluginspec ./
 
