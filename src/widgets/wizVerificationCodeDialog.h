a5ff0accd9546f40234db28de6b31cd9bef6ba59
WizLxn <lxn@wiz.cn>
2014-11-12 18:38:49 +0800

    注册验证码

--- src/widgets/wizVerificationCodeDialog.h
+++ src/widgets/wizVerificationCodeDialog.h
@@ -13,10 +13,24 @@ class CWizVerificationCodeDialog : public QDialog
 
 public:
     explicit CWizVerificationCodeDialog(QWidget *parent = 0);
-    ~CWizVerificationCodeDialog();
+    virtual ~CWizVerificationCodeDialog();
+
+    int verificationRequest(const QString& strUrl);
+    QString getVerificationCode() const;
+
+signals:
+    void verificationCodeInputed(const QString& strId);
+
+private slots:
+    void on_btn_image_clicked();
+
+    void on_btn_OK_clicked();
 
 private:
+    void downloadImage(QPixmap& pix);
+
     Ui::CWizVerificationCodeDialog *ui;
+    QString m_strUrl;
 };
 
 #endif // WIZVERIFICATIONCODEDIALOG_H
