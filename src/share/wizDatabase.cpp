a9d7896ae7897095629de74e8d47be3a9892d0ce
WizLxn <lxn@wiz.cn>
2014-11-20 18:23:19 +0800

    对非法的路径格式进行容错

--- src/share/wizDatabase.cpp
+++ src/share/wizDatabase.cpp
@@ -3208,20 +3208,30 @@ bool CWizDatabase::UpdateDocumentAbstract(const QString& strDocumentGUID)
 
 CString CWizDatabase::GetRootLocation(const CString& strLocation)
 {
-    int index = strLocation.indexOf('/', 1);
+    //FIXME:容错处理，如果路径的结尾不是 '/'，则增加该结尾符号
+    QString strLoc = strLocation;
+    if (strLoc.right(1) != "/")
+        strLoc.append('/');
+
+    int index = strLoc.indexOf('/', 1);
     if (index == -1)
         return CString();
 
-    return strLocation.left(index + 1);
+    return strLoc.left(index + 1);
 }
 
 CString CWizDatabase::GetLocationName(const CString& strLocation)
 {
-    int index = strLocation.lastIndexOf('/', strLocation.length() - 2);
+    //FIXME:容错处理，如果路径的结尾不是 '/'，则增加该结尾符号
+    QString strLoc = strLocation;
+    if (strLoc.right(1) != "/")
+        strLoc.append('/');
+
+    int index = strLoc.lastIndexOf('/', strLocation.length() - 2);
     if (index == -1)
         return CString();
 
-    CString str = strLocation.right(strLocation.length() - index - 1);
+    CString str = strLoc.right(strLocation.length() - index - 1);
 
     str.Trim('/');
 
