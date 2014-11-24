2b8e408686d77a72450f99cef55b3fa2a5cd70de
WizLxn <lxn@wiz.cn>
2014-11-24 15:57:43 +0800

    codesign on 10.9

--- mac-package-qt-5-appstore.sh
+++ mac-package-qt-5-appstore.sh
@@ -13,15 +13,15 @@ package_output_path="$HOME"
 mkdir ../WizQTClient-Release-QT5
 rm -rf ../WizQTClient-Release-QT5/* && \
 cd ../WizQTClient-Release-QT5 && \
-cmake -DWIZNOTE_USE_QT5=YES -DCMAKE_BUILD_TYPE=Release -UPDATE_TRANSLATIONS=YES -DAPPSTORE_BUILD=YES -DCMAKE_PREFIX_PATH=~/usr/local/qt/5.3.1/lib/cmake ../WizQTClient && \
+cmake -DWIZNOTE_USE_QT5=YES -DCMAKE_BUILD_TYPE=Release -UPDATE_TRANSLATIONS=YES -DAPPSTORE_BUILD=YES -DCMAKE_PREFIX_PATH=~/usr/local/qt/5.3.2/lib/cmake ../WizQTClient && \
 make -j5 
 
 MYAPP="WizNote"
 DEST="$MYAPP.app" # Our final App directory
  
-ICUDIR="/usr/local/icu53.1"
-ICULIBS="libicui18n.53 libicudata.53 libicuuc.53"
-QTDIR="/usr/local/qt/5.3.1"
+ICUDIR="/usr/local/icu54.1"
+ICULIBS="libicui18n.54 libicudata.54 libicuuc.54"
+QTDIR="/usr/local/qt/5.3.2"
 QTLIBS="QtCore QtNetwork QtSql QtGui QtOpenGL QtWidgets QtWebKit QtWebKitWidgets \
   QtPrintSupport QtXml QtPositioning QtSensors QtConcurrent QtMacExtras QtMultimediaWidgets QtMultimedia" # QtQml QtQuick QtSvg QtScript
 PLUGINS="sqldrivers imageformats  platforms printsupport accessible \
@@ -33,10 +33,14 @@ mkdir -p $DEST/Contents/Frameworks $DEST/Contents/PlugIns/icu $DEST/Contents/Sha
 # copy Qt libs, plug-ins and ICU
 for L in $QTLIBS ; do
   cp -R -p $QTDIR/lib/$L.framework $MYAPP.app/Contents/Frameworks
+  #mkdir $MYAPP.app/Contents/Frameworks/$L.framework/Resources
+  cp -R -p $MYAPP.app/Contents/Frameworks/$L.framework/Contents/Info.plist \
+    $MYAPP.app/Contents/Frameworks/$L.framework/Versions/5
+  rm -R -f $MYAPP.app/Contents/Frameworks/$L.framework/Contents
   # remove all unnecessary header files:
   rm -f $MYAPP.app/Contents/Frameworks/$L.framework/Headers
   rm -R -f $MYAPP.app/Contents/Frameworks/$L.framework/Versions/5/Headers
-  #rm $MYAPP.app/Contents/Frameworks/$L.framework/Versions/5/${L}_debug
+  rm $MYAPP.app/Contents/Frameworks/$L.framework/Versions/5/${L}_debug
 done
 for P in $PLUGINS ; do
   mkdir $MYAPP.app/Contents/PlugIns/$P
@@ -68,6 +72,15 @@ for I in $QTLIBS ; do
 done
  
 for P in $DISTPLUGINS ; do # change ID for all *.dylib libs
+
+  # remove debug file
+  result=$(echo $P | grep "_debug")
+  if [ "$result" != "" ];
+  then
+    rm $MYAPP.app/Contents/PlugIns/$P;
+    continue;
+  fi
+
   install_name_tool -id "@executable_path/../PlugIns/$I" "$MYAPP.app/Contents/PlugIns/$P"
   for L in $QTLIBS ; do # change any reference to Qt in our *.dylib libs
     install_name_tool -change $L.framework/Versions/5/$L\
@@ -100,10 +113,11 @@ for P in $DISTPLUGINS2 ; do # change ID for all *.dylib libs
 done
 
 
+DISTPLUGINS3=`cd $MYAPP.app/Contents/PlugIns; ls *.pluginspec`;
 
-install_name_tool -change libicui18n.53.dylib @executable_path/../PlugIns/icu/libicui18n.53.dylib WizNote.app/Contents/Frameworks/QtWebKit.framework/Versions/5/QtWebKit
-install_name_tool -change libicuuc.53.dylib @executable_path/../PlugIns/icu/libicuuc.53.dylib WizNote.app/Contents/Frameworks/QtWebKit.framework/Versions/5/QtWebKit
-install_name_tool -change libicudata.53.dylib @executable_path/../PlugIns/icu/libicudata.53.dylib WizNote.app/Contents/Frameworks/QtWebKit.framework/Versions/5/QtWebKit
+install_name_tool -change libicui18n.54.dylib @executable_path/../PlugIns/icu/libicui18n.54.dylib WizNote.app/Contents/Frameworks/QtWebKit.framework/Versions/5/QtWebKit
+install_name_tool -change libicuuc.54.dylib @executable_path/../PlugIns/icu/libicuuc.54.dylib WizNote.app/Contents/Frameworks/QtWebKit.framework/Versions/5/QtWebKit
+install_name_tool -change libicudata.54.dylib @executable_path/../PlugIns/icu/libicudata.54.dylib WizNote.app/Contents/Frameworks/QtWebKit.framework/Versions/5/QtWebKit
 install_name_tool -change $QTDIR/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore WizNote.app/Contents/Frameworks/QtXml.framework/Versions/5/QtXml
 
 
@@ -113,24 +127,30 @@ APPLCERT="3rd Party Mac Developer Application: Wei Shijun"
 INSTCERT="3rd Party Mac Developer Installer: Wei Shijun"
  
 for I in $QTLIBS ; do # signing the Qt frameworks
-  codesign -f -s "$APPLCERT" \
-    $MYAPP.app/Contents/Frameworks/$I.framework/Versions/5/$I
+  echo "codesing lib : " $MYAPP.app/Contents/Frameworks/$I.framework/Versions/5/$I;
+  codesign --force --verify --deep --verbose --sign "$APPLCERT" \
+    $MYAPP.app/Contents/Frameworks/$I.framework/Versions/5
 done
 for I in $DISTPLUGINS ; do # signing all *.dylib libs
   echo "code sign : "   $I;
-  codesign -f -s "$APPLCERT" \
+  codesign --force --verify --deep --verbose --sign "$APPLCERT" \
     $MYAPP.app/Contents/PlugIns/$I
 done
 
 for I in $DISTPLUGINS2 ; do # signing all *.dylib libs
   echo "code sign : "   $I;
-  codesign -f -s "$APPLCERT" \
+  codesign --force --verify --deep --verbose --sign "$APPLCERT" \
     $MYAPP.app/Contents/PlugIns/$I
 done
 
-codesign -f -s "$APPLCERT" -v --entitlements \
-  WizNote-Entitlements.plist \
-  "$MYAPP.app" --deep
+for I in $DISTPLUGINS3 ; do # signing all *.dylib libs
+  echo "code sign : "   $I;
+  codesign --force --verify --deep --verbose --sign "$APPLCERT" \
+    $MYAPP.app/Contents/PlugIns/$I
+done
+
+codesign --force --verify --verbose --sign "$APPLCERT" --entitlements \
+  WizNote-Entitlements.plist  "$MYAPP.app"
 
 productbuild --component "$MYAPP.app" /Applications \
   --sign "$INSTCERT" "$MYAPP.pkg"
