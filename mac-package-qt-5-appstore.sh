c4724b6fbf0eda7c7fd6a9ffdd8d3ecbefb80e06
WizLxn <lxn@wiz.cn>
2014-11-25 11:58:20 +0800

    签名时按照要求创建快捷方式

--- mac-package-qt-5-appstore.sh
+++ mac-package-qt-5-appstore.sh
@@ -7,18 +7,16 @@ volumn_path="/Volumes/$volumn_name"
 package_home="./macos-package"
 package_output_path="$HOME"
 
-
-
 # compile
 mkdir ../WizQTClient-Release-QT5
-rm -rf ../WizQTClient-Release-QT5/* && \
+rm -rf ../WizQTClient-Release-QT5/WizNote.app && \
 cd ../WizQTClient-Release-QT5 && \
 cmake -DWIZNOTE_USE_QT5=YES -DCMAKE_BUILD_TYPE=Release -UPDATE_TRANSLATIONS=YES -DAPPSTORE_BUILD=YES -DCMAKE_PREFIX_PATH=~/usr/local/qt/5.3.2/lib/cmake ../WizQTClient && \
 make -j5 
 
 MYAPP="WizNote"
 DEST="$MYAPP.app" # Our final App directory
- 
+BUILDDIR=$(pwd);
 ICUDIR="/usr/local/icu54.1"
 ICULIBS="libicui18n.54 libicudata.54 libicuuc.54"
 QTDIR="/usr/local/qt/5.3.2"
@@ -33,14 +31,25 @@ mkdir -p $DEST/Contents/Frameworks $DEST/Contents/PlugIns/icu $DEST/Contents/Sha
 # copy Qt libs, plug-ins and ICU
 for L in $QTLIBS ; do
   cp -R -p $QTDIR/lib/$L.framework $MYAPP.app/Contents/Frameworks
-  #mkdir $MYAPP.app/Contents/Frameworks/$L.framework/Resources
+  mkdir $MYAPP.app/Contents/Frameworks/$L.framework/Versions/5/Resources
   cp -R -p $MYAPP.app/Contents/Frameworks/$L.framework/Contents/Info.plist \
-    $MYAPP.app/Contents/Frameworks/$L.framework/Versions/5
+    $MYAPP.app/Contents/Frameworks/$L.framework/Versions/5/Resources
   rm -R -f $MYAPP.app/Contents/Frameworks/$L.framework/Contents
   # remove all unnecessary header files:
   rm -f $MYAPP.app/Contents/Frameworks/$L.framework/Headers
   rm -R -f $MYAPP.app/Contents/Frameworks/$L.framework/Versions/5/Headers
+  rm -R -f $MYAPP.app/Contents/Frameworks/$L.framework/Versions/Current
   rm $MYAPP.app/Contents/Frameworks/$L.framework/Versions/5/${L}_debug
+  rm $MYAPP.app/Contents/Frameworks/$L.framework/${L}
+  rm $MYAPP.app/Contents/Frameworks/$L.framework/${L}.prl
+  rm $MYAPP.app/Contents/Frameworks/$L.framework/${L}_debug
+  rm $MYAPP.app/Contents/Frameworks/$L.framework/${L}_debug.prl
+  cd $MYAPP.app/Contents/Frameworks/$L.framework/Versions
+  ln -s 5/ Current
+  cd ..
+  ln -s Versions/Current/$L $L
+  ln -s Versions/Current/Resources/ Resources
+  cd $BUILDDIR
 done
 for P in $PLUGINS ; do
   mkdir $MYAPP.app/Contents/PlugIns/$P
@@ -100,7 +109,6 @@ for L in $ICULIBS ; do
 done
 # we do the same for additional own libs in /Contents/PlugIns/myapp
 
-
 DISTPLUGINS2=`cd $MYAPP.app/Contents/PlugIns; ls *.dylib`
 
 for P in $DISTPLUGINS2 ; do # change ID for all *.dylib libs
@@ -110,6 +118,13 @@ for P in $DISTPLUGINS2 ; do # change ID for all *.dylib libs
       @executable_path/../Frameworks/$L.framework/Versions/5/$L\
       $MYAPP.app/Contents/PlugIns/$P
   done
+
+  for M in $DISTPLUGINS2 ; do
+    install_name_tool -change $BUILDDIR/WizNote.app/Contents/PlugIns/$M\
+      @executable_path/../PlugIns/$M\
+      $MYAPP.app/Contents/PlugIns/$P
+  done
+
 done
 
 
@@ -121,16 +136,27 @@ install_name_tool -change libicudata.54.dylib @executable_path/../PlugIns/icu/li
 install_name_tool -change $QTDIR/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore WizNote.app/Contents/Frameworks/QtXml.framework/Versions/5/QtXml
 
 
+install_name_tool -change $BUILDDIR/WizNote.app/Contents/PlugIns/libHelloWorld.dylib \
+ @executable_path/../PlugIns/libHelloWorld.dylib WizNote.app/Contents/MacOS/WizNote
+install_name_tool -change $BUILDDIR/WizNote.app/Contents/PlugIns/libMarkDown.dylib \
+ @executable_path/../PlugIns/libMarkDown.dylib WizNote.app/Contents/MacOS/WizNote
+install_name_tool -change $BUILDDIR/WizNote.app/Contents/PlugIns/libCore.dylib \
+ @executable_path/../PlugIns/libCore.dylib WizNote.app/Contents/MacOS/WizNote
+install_name_tool -change $BUILDDIR/WizNote.app/Contents/PlugIns/libextensionsystem.dylib \
+ @executable_path/../PlugIns/libextensionsystem.dylib WizNote.app/Contents/MacOS/WizNote 
+install_name_tool -change $BUILDDIR/WizNote.app/Contents/PlugIns/libaggregation.dylib \
+ @executable_path/../PlugIns/libaggregation.dylib WizNote.app/Contents/MacOS/WizNote 
+
+
 cp -R -p ../WizQTClient/build/osx/WizNote-Entitlements.plist WizNote-Entitlements.plist
 
 APPLCERT="3rd Party Mac Developer Application: Wei Shijun"
 INSTCERT="3rd Party Mac Developer Installer: Wei Shijun"
  
-for I in $QTLIBS ; do # signing the Qt frameworks
-  echo "codesing lib : " $MYAPP.app/Contents/Frameworks/$I.framework/Versions/5/$I;
-  codesign --force --verify --deep --verbose --sign "$APPLCERT" \
-    $MYAPP.app/Contents/Frameworks/$I.framework/Versions/5
-done
+# for I in $QTLIBS ; do # signing the Qt frameworks
+#   codesign --force --verify --deep --verbose --sign "$APPLCERT" \
+#     $MYAPP.app/Contents/Frameworks/$I.framework/Versions/5
+# done
 for I in $DISTPLUGINS ; do # signing all *.dylib libs
   echo "code sign : "   $I;
   codesign --force --verify --deep --verbose --sign "$APPLCERT" \
@@ -149,7 +175,7 @@ for I in $DISTPLUGINS3 ; do # signing all *.dylib libs
     $MYAPP.app/Contents/PlugIns/$I
 done
 
-codesign --force --verify --verbose --sign "$APPLCERT" --entitlements \
+codesign --deep --force --verify --verbose --sign "$APPLCERT" --entitlements \
   WizNote-Entitlements.plist  "$MYAPP.app"
 
 productbuild --component "$MYAPP.app" /Applications \
