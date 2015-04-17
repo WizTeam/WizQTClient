9602161c44df48a63a57ae12e86eaeb04cbe1798
WizLxn <lxn@wiz.cn>
2015-04-17 10:08:51 +0800

    将列表样式提取为模板

--- src/wiznotestyle.h
+++ src/wiznotestyle.h
@@ -3,6 +3,9 @@
 
 #include <QString>
 #include <QStyle>
+#include <QProxyStyle>
+#include <QListWidget>
+#include <QListWidgetItem>
 
 QStyle* WizGetStyle(const QString& skinName);
 
@@ -11,4 +14,38 @@ QStyle* WizGetImageButtonStyle(const QString& normalBackgroundFileName, const QS
                                const QColor& normalTextColor, const QColor& activeTextColor, const QColor& disableTextColor);
 
 
+
+template <typename T>
+class CWizListItemStyle : public QProxyStyle
+{
+public:
+    CWizListItemStyle(QStyle *style = 0) : QProxyStyle(style) {}
+    virtual void drawControl(ControlElement element, const QStyleOption *option, QPainter *painter, const QWidget *widget) const
+    {
+        switch (element)
+        {
+        case CE_ItemViewItem:
+            {
+                const QStyleOptionViewItemV4 *vopt = qstyleoption_cast<const QStyleOptionViewItemV4 *>(option);
+                Q_ASSERT(vopt);
+
+                if (const QListWidget* view = dynamic_cast<const QListWidget*>(widget))
+                {
+                    QListWidgetItem *item = view->item(vopt->index.row());
+                    if (item)
+                    {
+                        if (T* templateItem = dynamic_cast<T*>(item))
+                        {
+                            templateItem->draw(painter, vopt);
+                        }
+                    }
+                }
+                break;
+            }
+        default:
+            QProxyStyle::drawControl(element, option, painter, widget);
+            break;
+        }
+    }
+};
 #endif // WIZNOTESTYLE_H
