038ac56ff41e0fa32cb03d3239376969c1ce79a6
WizLxn <lxn@wiz.cn>
2015-09-15 20:22:35 +0800

    粘贴文本时不换行的问题

--- share/files/editor/ueditor.all.js
+++ share/files/editor/ueditor.all.js
@@ -14180,7 +14180,7 @@ UE.plugins['paste'] = function () {
         //trace:717 隐藏的span不能得到top
         //bk.start.innerHTML = '&nbsp;';
         bk.start.style.display = '';
-        pastebin.style.cssText = "position:absolute;width:1px;height:1px;overflow:hidden;left:-1000px;top:" +
+        pastebin.style.cssText = "position:absolute;width:1px;height:1px;overflow:hidden;left:-1000px;white-space:nowrap;top:" +
             //要在现在光标平行的位置加入，否则会出现跳动的问题
             domUtils.getXY(bk.start).y + 'px';
 
