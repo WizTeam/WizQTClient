e2582ca5d63815476ce9f0f8333fb2da449d640d
WizLxn <lxn@wiz.cn>
2015-10-16 18:34:37 +0800

    评论数目

--- src/cellbutton.cpp
+++ src/cellbutton.cpp
@@ -112,7 +112,7 @@ void CellButton::paintEvent(QPaintEvent *event)
     if (WithCountInfo == m_buttonType)
     {
         QRect rcText(rcIcon.right() + 6, opt.rect.y(), opt.rect.width() - rcIcon.width(), opt.rect.height());
-        p.setPen(QColor("#B6B6B6"));
+        p.setPen(m_count == 0 ? QColor("#B6B6B6") : QColor("#5990EF"));
         p.drawText(rcText,Qt::AlignVCenter | Qt::AlignLeft, countInfo());
     }
 }
